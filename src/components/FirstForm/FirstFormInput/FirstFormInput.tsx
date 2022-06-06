8f689ae6ab61944b3ea9c799be0abd55e0a9fd0f
maximLomako <maxim.lomako@icloud.com>
2022-06-06 22:59:54 +0400

    first commit

---
+++ src/components/FirstForm/FirstFormInput/FirstFormInput.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FirstFormInput = () => {
+ return (
+   <div>
+     <label htmlFor="1">Title</label>
+     <input type='text' name='first-field' id='1'/>
+   </div>
+ )
+}
+
+export default FirstFormInput;
